e07a29beec560ebe774fe8bd60ae8a9442bc5bee
Daniel Lewis <daniel.robert.lewis@gmail.com>
2023-03-22 22:27:00 -0700

    test release

--- test.sh
+++ test.sh
@@ -2,13 +2,15 @@
 set -x
 
 
-inc=$(cat inc)
-inc=$[$inc+1]
-echo $inc > inc
-version="v0.0.16_test$inc"
+# increment build number
+current_version="$(cat VERSION)"
+v=$[`echo $current_version | grep -oE "[0-9]+$"`+1]
+new_v="`echo "$current_version" | sed "s~[0-9]*$~$v~"`"
+version="$new_v"
+echo $version > VERSION
 
 ./copy_from_kicad.sh
 git add .
 git commit -m "test release"
-git tag -a $version -m $version
+git tag -a v$version -m v$version
 git push origin master --tags
