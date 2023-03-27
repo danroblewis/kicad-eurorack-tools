a2423d8b4be359fd744bbabb0e3f6b53d15c9724
Daniel Lewis <daniel.robert.lewis@gmail.com>
2023-03-26 18:03:42 -0700

    new version

--- easy.sh
+++ easy.sh
@@ -3,6 +3,7 @@
 git pull origin master
 ./copy_from_kicad.sh
 dos2unix library/*/*kicad_sym
+dos2unix plugin/*py
 git diff
 echo -n "Looks good?"
 read
--- plugin/draw_panel.py
+++ plugin/draw_panel.py
@@ -1,133 +1,85 @@
-import pcbnew
+import datetime
+import logging
+import os
+import re
+import sys
 
-def nnline(a, b, stroke=0.5):
-	box = pcbnew.PCB_SHAPE()
-	box.SetLayer(pcbnew.Edge_Cuts)
-	box.SetShape(pcbnew.S_SEGMENT)
-	box.SetStart(pcbnew.VECTOR2I(int(a[0]*pcbnew.schIUScale.mmToIU(100)),int(a[1]*pcbnew.schIUScale.mmToIU(100))))
-	box.SetEnd(pcbnew.VECTOR2I(int(b[0]*pcbnew.schIUScale.mmToIU(100)),int(b[1]*pcbnew.schIUScale.mmToIU(100))))
-	box.SetWidth(int(stroke*pcbnew.schIUScale.mmToIU(100)))
-	pcbnew.GetBoard().Add(box)
+import wx
+import wx.dataview
+from pcbnew import GetBoard
 
+from .helpers import (
+    PLUGIN_PATH,
+    GetScaleFactor,
+    HighResWxSize,
+    get_footprint_by_ref,
+    getVersion,
+    loadBitmapScaled,
+    toggle_exclude_from_bom,
+    toggle_exclude_from_pos,
+)
 
-def nnrect(pos, dims, stroke=0.5):
-	box = pcbnew.PCB_SHAPE()
-	box.SetLayer(pcbnew.Edge_Cuts)
-	box.SetShape(pcbnew.S_RECT)
-	box.SetStart(pcbnew.VECTOR2I(int(pos[0]*pcbnew.schIUScale.mmToIU(100)), int(pos[1]*pcbnew.schIUScale.mmToIU(100))))
-	box.SetEnd(pcbnew.VECTOR2I(int((pos[0]+dims[0])*pcbnew.schIUScale.mmToIU(100)), int((pos[1]+dims[1])*pcbnew.schIUScale.mmToIU(100))))
-	box.SetWidth(int(stroke*pcbnew.schIUScale.mmToIU(100)))
-	pcbnew.GetBoard().Add(box)
+from .draw_panel import draw_euro_panel, draw_euro_frontpanel
 
-
-
-def nnzone(pos, dims, layername):
-	board = pcbnew.GetBoard()
-	nets = board.GetNetsByName()
-	gnd_net = nets.find("GND").value()[1]  # this is a NETINFO_ITEM
-
-	layertable = {}
-	for i in range(pcbnew.PCB_LAYER_ID_COUNT):
-	    layertable[board.GetLayerName(i)] = i  # this is an integer
-
-	newarea = board.AddArea(
-		None, 
-		gnd_net.GetNetCode(), 
-		layertable[layername], 
-		pcbnew.VECTOR2I(int(pos[0]*pcbnew.schIUScale.mmToIU(100)),int(pos[1]*pcbnew.schIUScale.mmToIU(100))), 
-		pcbnew.ZONE_BORDER_DISPLAY_STYLE_DIAGONAL_EDGE
-	)
-	newoutline = newarea.Outline()
-	# newoutline.Append(int(pos[0]*pcbnew.schIUScale.mmToIU(100)),int(pos[1]*pcbnew.schIUScale.mmToIU(100)))
-	newoutline.Append(int((pos[0]+dims[0])*pcbnew.schIUScale.mmToIU(100)),int(pos[1]*pcbnew.schIUScale.mmToIU(100)))
-	newoutline.Append(int((pos[0]+dims[0])*pcbnew.schIUScale.mmToIU(100)),int((pos[1]+dims[1])*pcbnew.schIUScale.mmToIU(100)))
-	newoutline.Append(int(pos[0]*pcbnew.schIUScale.mmToIU(100)),int((pos[1]+dims[1])*pcbnew.schIUScale.mmToIU(100)))
-	newarea.HatchBorder()
+class EurorackTools(wx.Dialog):
+    def __init__(self, parent):
+        wx.Dialog.__init__(
+            self,
+            parent,
+            id=wx.ID_ANY,
+            title=f"Eurorack Tools [  ]",
+            pos=wx.DefaultPosition,
+            size=wx.Size(230, 100),
+            style=wx.DEFAULT_DIALOG_STYLE | wx.RESIZE_BORDER | wx.MAXIMIZE_BOX,
+        )
+        self.window = wx.GetTopLevelParent(self)
+        self.SetSize(HighResWxSize(self.window, wx.Size(230, 100)))
+        self.scale_factor = GetScaleFactor(self.window)
+        self.project_path = os.path.split(GetBoard().GetFileName())[0]
+        self.Bind(wx.EVT_CLOSE, self.quit_dialog)
 
 
-def nncircle(pos, diameter, stroke=0.5):
-	circle = pcbnew.PCB_SHAPE()
-	circle.SetLayer(pcbnew.Edge_Cuts)
-	circle.SetShape(pcbnew.S_CIRCLE)
-	pts = [
-		(pos[0],pos[1]),
-		(pos[0],pos[1]+diameter/2.0)
-	]
+        self.hpbox = wx.TextCtrl(
+            self,
+            wx.ID_ANY,
+            "4",
+            wx.Point(50,2),
+            wx.DefaultSize,
+        )
 
-	circle.SetStart(pcbnew.VECTOR2I(int(pts[0][0]*pcbnew.schIUScale.mmToIU(100)), int(pts[0][1]*pcbnew.schIUScale.mmToIU(100))))
-	circle.SetEnd(pcbnew.VECTOR2I(int(pts[1][0]*pcbnew.schIUScale.mmToIU(100)), int(pts[1][1]*pcbnew.schIUScale.mmToIU(100))))
+        self.btn_draw_panel = wx.Button(
+            self,
+            wx.ID_ANY,
+            "Draw Panel",
+            wx.Point(5,30), # wx.DefaultPosition,
+            HighResWxSize(self.window, wx.Size(100, -1)),
+            0,
+        )
 
-	circle.SetWidth(int(stroke*pcbnew.schIUScale.mmToIU(100)))
-	pcbnew.GetBoard().Add(circle)
+        self.btn_draw_frontpanel = wx.Button(
+            self,
+            wx.ID_ANY,
+            "Draw Frontpanel",
+            wx.Point(110,30), # wx.DefaultPosition,
+            HighResWxSize(self.window, wx.Size(100, -1)),
+            0,
+        )
 
+        self.hpbox.Bind(wx.EVT_TEXT_ENTER, self.drawpanel)
+        self.btn_draw_panel.Bind(wx.EVT_BUTTON, self.drawpanel)
+        self.btn_draw_frontpanel.Bind(wx.EVT_BUTTON, self.drawfrontpanel)
 
-def draw_euro_panel(hp, snaps=True):
-	onehp = 5.08
+    def drawpanel(self, e):
+        hpwidth = int(self.hpbox.GetValue())
+        draw_euro_panel(hpwidth)
+        self.quit_dialog(None)
 
-	ax = 20
-	ay = 20
-	width = hp*onehp
-	height = 128.5
-	screwsize = 3.2
-	betweenscrews = 122.5
-	screwtopoffset = 3
+    def drawfrontpanel(self, e):
+        hpwidth = int(self.hpbox.GetValue())
+        draw_euro_frontpanel(hpwidth)
+        self.quit_dialog(None)
 
-	board = pcbnew.GetBoard()
-
-	nnrect((ax, ay), (width, height))
-	nnzone((ax, ay), (width, height), 'F.Cu')
-	nnzone((ax, ay), (width, height), 'B.Cu')
-
-	# there should be 2 or 4 screw holes. if the module is 2hp, just draw them in the middle
-	if hp <= 2:
-		nncircle((ax + width/2, ay + screwtopoffset), screwsize)
-		nncircle((ax + width/2, ay + screwtopoffset + betweenscrews), screwsize)
-	else:
-		# the edge of the circle should be 5.08mm from the edge of the board
-		# so the center of the circle should be edge + 5.08 + screwsize/2
-		space_from_edge = onehp + screwsize/2
-		nncircle((ax + space_from_edge, ay + screwtopoffset), screwsize)
-		nncircle((ax + width - space_from_edge, ay + screwtopoffset), screwsize)
-		nncircle((ax + space_from_edge, ay + screwtopoffset + betweenscrews), screwsize)
-		nncircle((ax + width - space_from_edge, ay + screwtopoffset + betweenscrews), screwsize)
-
-	if snaps:
-		backplaneheight = 109
-		bx = ax
-		by = ay + int((height-backplaneheight)/2)
-
-		# space out cut-lines every 1hp
-		# that's 2mm connected, 3mm not connected
-		# that's 5mm wide each time
-		cut_spacing = 5
-		for ly in [by, by+backplaneheight]:
-			for lx in range(int(ax), int(ax+width-4), 5):
-				nnline(
-					(1.5+lx, ly),
-					(1.5+lx + 3, ly)
-				)
-
-	# TODO: widen the mounting holes with arcs+lines so they fit better with any case design.
-	# TODO: group all of the objects so people can delete them easier
-
-	pcbnew.Refresh()
-
-
-# TODO: write a function to create a frontpanel to the right with mounting holes in the appropriate places for the pots, jacks, and leds
-def draw_euro_frontpanel(hp):
-	board = pcbnew.GetBoard()
-	fs = board.GetFootprints()
-	pots = [ f for f in fs if 'Potentiometer' in f.GetDescription() ]
-	jacks = [ f for f in fs if 'Thonkiconn' in f.GetDescription() ]
-
-	for jack in jacks:
-		c = jack.GetCenter()
-		x = c[0] / pcbnew.schIUScale.mmToIU(100)
-		y = c[1]  / pcbnew.schIUScale.mmToIU(100)
-		# TODO: find the rotation of the jack, adjust circle position accordingly
-		nncircle((x+0.7,y), 5)
-
-	pcbnew.Refresh()
-
-
-
+    def quit_dialog(self, e):
+        """Destroy dialog on close"""
+        self.Destroy()
+        self.EndModal(0)
